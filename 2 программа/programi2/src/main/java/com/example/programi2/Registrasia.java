6729d3d1168f954433bdaf6d16b4b8eeed82c991
Ermaditov <166543645+Ermaditov@users.noreply.github.com>
2025-09-17 13:54:25 +0300

    Delete 2 программа directory

--- 2 программа/programi2/src/main/java/com/example/programi2/Registrasia.java
+++
@@ -1,4 +0,0 @@
-package com.example.programi2;
-
-public class Registrasia {
-}
--- 2 программа/programi2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.programi2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.programi2 to javafx.fxml;
-    exports com.example.programi2;
-}
